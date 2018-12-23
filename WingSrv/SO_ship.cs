99796203a31009cbec7666bf06a960d3eaba9e4c
algor1 <alexeev.igor.v@gmail.com>
2018-12-23 23:36:36 +0300

    first commit

---
+++ WingSrv/SO_ship.cs
@@ -0,0 +1,521 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace WingSrv
+{
+
+    public class SO_ship
+    {
+
+
+        public SO_shipData p; //ship properties	
+        public GameObject host;
+
+        public bool atack;
+        private Quaternion rotationToTarget;
+
+        private ServerObject target;
+        public ServerObject targetToMove;
+        public ServerObject newtargetToMove;
+        //	private Quaternion oldRotation; //for roll calc
+
+        private ServerObject targetToAtack;
+        private ServerObject newtargetToAtack;
+        public enum MoveType { move, warp, stop };
+        public MoveType moveCommand;
+        public enum ComandType { warpTo, goTo, landTo, none, open };
+        public ComandType complexCommand;
+        public enum ShipEvenentsType { spawn, warp, warmwarp, move, stop, land, hide, reveal, destroyed, open };
+        public bool warpActivated;
+        public bool warpCoroutineStarted;
+        public bool landed;
+
+        public List<SO_weapon> weapons;
+        public List<SO_equipment> equipments;
+
+
+        public SO_ship(SO_shipData shipData, GameObject _host)
+        {
+
+            p = shipData;
+            rotationToTarget = p.SO.rotation;
+            moveCommand = MoveType.move;
+            SendEvent(ShipEvenentsType.move);
+
+            host = _host;
+            Debug.Log(host);
+
+            newtargetToMove = null;
+            weapons = new List<SO_weapon>();
+            for (int i = 0; i < shipData.weapons.Count; i++)
+            {
+                Debug.Log(p.SO.id + " ---init  weapon  " + i + "    " + shipData.weapons[i].type + shipData.weapons.Count);
+                SO_weapon newweappon = new SO_weapon(shipData.weapons[i], this);
+                weapons.Add(newweappon);
+            }
+            equipments = new List<SO_equipment>();
+            for (int i = 0; i < shipData.equipments.Count; i++)
+            {
+                Debug.Log(p.SO.id + " ---init  active equipment  " + i + "    " + shipData.equipments.Count);
+                SO_equipment neweq = new SO_equipment(shipData.equipments[i], this);
+                equipments.Add(neweq);
+            }
+            this.p.SO.ship = this;
+
+        }
+        public void BeforeDestroy()
+        {
+            for (int i = 0; i < weapons.Count; i++)
+            {
+                weapons[i].BeforeDestroy();
+                weapons.RemoveAt(i);
+            }
+            for (int i = 0; i < equipments.Count; i++)
+            {
+                equipments[i].BeforeDestroy();
+                equipments.RemoveAt(i);
+
+            }
+        }
+        public void SetTarget(ServerObject newtarget)
+        {
+            Debug.Log("new target  " + newtarget.visibleName);
+            newtargetToMove = newtarget;
+            newtargetToAtack = newtarget;
+            Debug.Log(p.SO.visibleName + " p" + p.SO.position + " set target to " + newtargetToAtack.visibleName + " p " + newtargetToAtack.position);
+            //		newtargetToAtack=null;
+        }
+        //	public void SetTarget(SO_ship newtarget){	
+        ////		Debug.Log ("new target  " + newtarget.p.visibleName);
+        //		newtargetToMove = newtarget.p.SO;
+        //		newtargetToAtack=newtarget;
+        //		Debug.Log (p.SO.visibleName +" p"+p.SO.position+ " set target to " + newtargetToAtack.visibleName+" p "+newtargetToAtack.position);
+        ////		Debug.Log (newtargetToMove.position);
+        //
+        //	}
+        public void GoToTarget()
+        {
+            if (newtargetToMove != null)
+            {
+
+                complexCommand = ComandType.goTo;
+                targetToMove = newtargetToMove;
+                //			oldRotation = p.SO.rotation;
+
+
+            }
+
+        }
+        public void WarpToTarget()
+        {
+            if (newtargetToMove != null && Vector3.Distance(p.SO.position, newtargetToMove.position) > 100000)
+            {
+                Debug.Log("warping");
+                moveCommand = MoveType.warp;
+                complexCommand = ComandType.warpTo;
+                targetToMove = newtargetToMove;
+                //			oldRotation = p.SO.rotation;
+                warpActivated = false;
+            }
+            else
+            {
+                Debug.Log("TO CLOSE TO WARP");
+            }
+
+        }
+        public void StartEquipment()
+        {
+            for (int i = 0; i < equipments.Count; i++)
+            {
+                if (!equipments[i].activate)
+                {
+                    equipments[i].Use();
+                }
+                else
+                {
+                    equipments[i].Stop();
+                }
+            }
+
+        }
+        public void StopEquipment()
+        {
+            for (int i = 0; i < equipments.Count; i++)
+            {
+                equipments[i].Stop();
+
+            }
+
+        }
+
+        public void LandToTarget()
+        {
+            if (newtargetToMove != null)
+            {
+                complexCommand = ComandType.landTo;
+                targetToMove = newtargetToMove;
+                //            oldRotation = p.SO.rotation;
+                //			Debug.Log (targetToMove.position);
+            }
+        }
+        public void OpenTarget()
+        {
+            if (newtargetToMove != null)
+            {
+                complexCommand = ComandType.open;
+                targetToMove = newtargetToMove;
+                //            oldRotation = p.SO.rotation;
+                //			Debug.Log (targetToMove.position);
+            }
+        }
+
+        public bool Rotate()
+        {
+            if (targetToMove != null)
+            {
+                //			Debug.Log ("id " + p.id + "targettomove " + targetToMove.position + "  p  " + p.SO.position);
+                rotationToTarget = Quaternion.LookRotation(targetToMove.position - p.SO.position);
+                Vector3 rt = new Vector3(rotationToTarget.eulerAngles.x, rotationToTarget.eulerAngles.y, p.SO.rotation.eulerAngles.z);
+                rotationToTarget = Quaternion.Euler(rt);
+
+                if (Mathf.Abs(p.SO.rotation.eulerAngles.x - rotationToTarget.eulerAngles.x) > 1 || Mathf.Abs(p.SO.rotation.eulerAngles.y - rotationToTarget.eulerAngles.y) > 1)
+                {
+                    //				Debug.Log ("id " + p.id + "  rotating " + p.rotation.eulerAngles + "  to  " + rotationToTarget.eulerAngles);
+                    //				Debug.Log ("id " + p.id + "  position " + p.SO.position + "  target pos" + targetToMove.position);
+                    //				Debug.Log("sin z " +  Mathf.Sin(Mathf.Deg2Rad*p.rotation.z)+ "  cos z " +  Mathf.Cos(Mathf.Deg2Rad*p.rotation.z));
+
+                    p.SO.rotation = Quaternion.RotateTowards(p.SO.rotation, rotationToTarget, p.rotation_speed * Time.deltaTime);
+                    //				float difx = p.rotation.eulerAngles.x - oldRotation.eulerAngles.x;
+                    //				float dify = p.rotation.eulerAngles.y - oldRotation.eulerAngles.y;
+                    //				Debug.Log("dif x " +  (difx*Mathf.Sin(Mathf.Deg2Rad*p.rotation.z)) + "  dif y " + (dify*Mathf.Cos(Mathf.Deg2Rad*p.rotation.z)) );
+                    //				Debug.Log("roll" +(difx*Mathf.Sin(Mathf.Deg2Rad*p.rotation.z) + dify*Mathf.Cos(Mathf.Deg2Rad*p.rotation.z)) );
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        void Accelerate()
+        {
+            if (p.newSpeed != p.SO.speed)
+            {
+                if (p.newSpeed > p.SO.speed)
+                {
+                    p.SO.speed += Time.deltaTime * p.acceleration_max;
+                    if (p.newSpeed < p.SO.speed)
+                    {
+                        p.SO.speed = p.newSpeed;
+                    }
+                }
+                else
+                {
+                    p.SO.speed += -Time.deltaTime * p.acceleration_max;
+                    if (p.newSpeed > p.SO.speed)
+                    {
+                        p.SO.speed = p.newSpeed;
+                    }
+                }
+            }
+        }
+
+        private void Move()
+        {
+            if (moveCommand == MoveType.move)
+            {
+                Rotate();
+                Accelerate();
+                p.SO.speed = Mathf.Lerp(p.SO.speed, p.max_speed, Time.deltaTime * p.acceleration_max);
+                p.SO.position += p.SO.rotation * Vector3.forward * Time.deltaTime * p.SO.speed;
+            }
+        }
+
+
+        private void Stop()
+        {
+            if (moveCommand == MoveType.stop)
+            {
+                //			SendEvent(ShipEvenentsType.stop);
+                p.newSpeed = 0;
+            }
+        }
+
+        public void Atack_target(int weaponnum)
+        {
+            //		Debug.Log ("atacking");
+            if (newtargetToAtack != null)
+            {
+                targetToAtack = newtargetToAtack;
+                Debug.Log(p.SO.visibleName + " atacking " + targetToAtack.visibleName);
+
+                atack = true;
+                weapons[weaponnum].Atack_target(targetToAtack);
+
+            }
+            else
+            {
+                atack = false;
+            }
+
+        }
+        public void StopFire(int weaponnum)
+        {
+            Debug.Log(p.SO.visibleName + " stop fire ");
+            atack = false;
+            weapons[weaponnum].stop();
+        }
+
+
+
+
+        public void Damage(float damage)
+        {
+            Debug.Log(p.SO.visibleName + " damage recieved");
+            if (p.shield - damage > 0)
+            {
+                p.shield += -damage;
+            }
+            else
+            {
+                if (p.shield + p.armor - damage > 0)
+                {
+                    p.armor += p.shield - damage;
+                    p.shield = 0;
+                }
+                else
+                {
+                    if (p.shield + p.armor + p.hull - damage > 0)
+                    {
+                        p.hull += p.armor + p.shield - damage;
+                        p.shield = 0;
+                        p.armor = 0;
+                    }
+                    else
+                    {
+                        Destroyed();
+                    }
+                }
+            }
+        }
+        private void Destroyed()
+        {
+            p.destroyed = true;
+            for (int i = 0; i < weapons.Count; i++)
+            {
+                StopFire(i);
+            }
+            StopEquipment();
+            SendEvent(ShipEvenentsType.destroyed);
+        }
+
+        private void RestoreTick()
+        {
+            if (p.hull < p.hull_full)
+            {
+                p.hull += p.hull_restore * Time.deltaTime;
+            }
+            else { p.hull = p.hull_full; }
+            if (p.shield < p.shield_full)
+            { p.shield += p.shield_restore * Time.deltaTime; }
+            else { p.shield = p.shield_full; }
+
+            if (p.armor < p.armor_full) { p.armor += p.armor_restore * Time.deltaTime; }
+            else { p.armor = p.armor_full; }
+            if (p.capasitor < p.capasitor_full) { p.capasitor += p.capasitor_restore * Time.deltaTime; }
+            else { p.capasitor = p.capasitor_full; }
+
+        }
+        private void Agr()
+        {
+            if (p.mob && !p.destroyed)
+            {
+                //			Debug.Log ("mob "+p.id);
+                if (newtargetToAtack != null && !atack)
+                {
+                    //				Debug.Log ("mob "+p.id+" targ "+newtargetToAtack.p.id +" dist " + Vector3.Distance(p.SO.position, newtargetToAtack.p.SO.position)+" adist "+ p.agr_distance);
+                    if (Vector3.Distance(p.SO.position, newtargetToAtack.position) < p.agr_distance)
+                    {
+                        //					Debug.Log ("agr");
+                        SetTarget(newtargetToAtack);
+                        GoToTarget();
+                        for (int i = 0; i < weapons.Count; i++)
+                        {
+                            Atack_target(i);
+                        }
+                    }
+                    else
+                    {
+                        atack = false;
+                    }
+                }
+            }
+        }
+        public void SendEvent(ShipEvenentsType evnt)
+        {
+            if (host != null)
+            {
+                //			Debug.Log (host.name);
+                if (host.name == "ServerGo")
+                {
+                    host.GetComponent<Server>().Events(evnt, this);
+
+                }
+                else
+                {
+                    host.GetComponent<ShipMotor>().Events(evnt, this);
+                }
+                //			Debug.Log (host.name);
+
+            }
+        }
+
+        public IEnumerator Warpdrive()
+        {
+            warpCoroutineStarted = true;
+            SendEvent(ShipEvenentsType.warmwarp);
+            Debug.Log("warming warp drive  " + p.warpDriveStartTime + " s");
+            yield return new WaitForSeconds(p.warpDriveStartTime);
+
+
+            float warpDistance = Vector3.Distance(p.SO.position, targetToMove.position);
+
+            float warpTime = warpDistance / p.warpSpeed;
+            SendEvent(ShipEvenentsType.hide);
+            Hide();
+            warpActivated = true;
+            Debug.Log("flying warp   " + warpTime + " s");
+            SendEvent(ShipEvenentsType.warp);
+            yield return new WaitForSeconds(warpTime);
+
+            Spawn(targetToMove.position - Vector3.forward * 10);
+            SendEvent(ShipEvenentsType.spawn);
+            SendEvent(ShipEvenentsType.reveal);
+            Reveal();
+            warpActivated = false;
+            //warpToTargetFlag = false;
+            warpCoroutineStarted = false;
+            p.SO.speed = p.max_speed * 0.1f;
+            moveCommand = MoveType.stop;
+            SendEvent(ShipEvenentsType.stop);
+
+            complexCommand = ComandType.none;
+
+
+        }
+
+        private void Spawn(Vector3 _position)
+        {
+            p.SO.position = _position;
+            Debug.Log(host);
+            if (host != null)
+            {
+                //			host.GetComponent<ShipMotor>().Spawn(p.SO.id);
+            }
+        }
+        private void Hide()
+        {
+            p.hidden = true;
+        }
+        private void Reveal()
+        {
+            p.hidden = false;
+        }
+
+
+        private void CommandManager()
+        {
+            if (complexCommand == ComandType.goTo)
+            {
+
+                if (Vector3.Distance(p.SO.position, targetToMove.position) > 10 * p.SO.speed / p.acceleration_max)
+                {
+                    //				Debug.Log ("command goto");
+                    moveCommand = MoveType.move;
+                    SendEvent(ShipEvenentsType.move);
+
+                }
+                else
+                {
+                    //				Debug.Log(Vector3.Distance (p.SO.position, targetToMove.position));
+
+                    moveCommand = MoveType.stop;
+                    SendEvent(ShipEvenentsType.stop);
+
+                    complexCommand = ComandType.none;
+                }
+            }
+            if (complexCommand == ComandType.warpTo)
+            {
+                moveCommand = MoveType.warp;
+            }
+            if (complexCommand == ComandType.landTo)
+            {
+                if (Vector3.Distance(p.SO.position, targetToMove.position) > 10 * p.max_speed / p.acceleration_max)
+                {
+                    moveCommand = MoveType.move;
+                    SendEvent(ShipEvenentsType.move);
+
+                }
+                else
+                {
+
+                    moveCommand = MoveType.stop;
+                    SendEvent(ShipEvenentsType.stop);
+
+
+                    SendEvent(ShipEvenentsType.land);
+                    complexCommand = ComandType.none;
+
+                }
+
+            }
+            if (complexCommand == ComandType.open)
+            {
+                if (Vector3.Distance(p.SO.position, targetToMove.position) > 10 * p.max_speed / p.acceleration_max)
+                {
+                    moveCommand = MoveType.move;
+                    SendEvent(ShipEvenentsType.move);
+
+                }
+                else
+                {
+
+                    moveCommand = MoveType.stop;
+                    SendEvent(ShipEvenentsType.stop);
+                    SendEvent(ShipEvenentsType.open);
+                    complexCommand = ComandType.none;
+
+                }
+
+            }
+
+
+        }
+
+
+
+
+        public void Tick()
+        {
+            Agr();
+            RestoreTick();
+            CommandManager();
+            Move();
+            Stop();
+        }
+
+        //	public ServerObject ConvertToSO(){
+        //		ServerObject SO= new ServerObject();
+        //		SO.id=p.SO.id;
+        //		SO.position=p.SO.position;
+        //		SO.rotation=p.SO.rotation;
+        //		SO.speed=p.SO.speed;
+        //		SO.prefab=p.SO.prefab;
+        //		SO.visibleName = p.SO.visibleName;
+        //		return SO;
+        //	}
+
+    }
+}
---
+++ WingSrv/Server.cs
@@ -0,0 +1,320 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace WingSrv
+{
+
+    public class Server : MonoBehaviour
+    {
+        public bool started;
+        private List<SO_ship> ships;
+
+        public enum Command { MoveTo, WarpTo, Atack, SetTarget, LandTo, Equipment, Open, TakeOff };
+        public enum ShipEvenentsType { spawn, warp, warmwarp, move, stop, land, hide, reveal };
+
+
+
+        void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            ships = new List<SO_ship>();
+            LoadShips();
+        }
+
+        void Start()
+        {
+            ships[1].SetTarget(ships[0].p.SO);
+            ships[1].GoToTarget();
+            started = true;
+
+
+        }
+
+
+        void FixedUpdate()
+        {
+            Tick();
+
+        }
+
+
+
+
+        public List<SO_ship> Nearest(int player_id)
+        {
+            SO_ship pl = GetPlayer(player_id);
+            List<SO_ship> resultShipList = new List<SO_ship>();
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if (ships[i].p.SO.id == player_id || ships[i].p.hidden) continue;//remove player from list
+                if (ships[i].p.SO.id == player_id || ships[i].p.destroyed) continue;//remove player from list
+
+                float dist = Vector3.Distance(pl.p.SO.position, ships[i].p.SO.position);
+                //			print (dist);
+                if (dist < pl.p.vision_distance)
+                {
+                    resultShipList.Add(ships[i]);
+                }
+                if (dist < ships[i].p.vision_distance && !ships[i].atack)
+                {
+                    ships[i].SetTarget(pl.p.SO);//playerid=0
+                }
+            }
+            //		if (resultShipList.Count>0)
+            //		Debug.Log ("nearest "+resultShipList.Count);
+
+            return resultShipList;
+        }
+
+        public SO_ship GetPlayer(int player_id)
+        {
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if (ships[i].p.SO.id == player_id) return ships[i];
+            }
+            return ships[0];
+
+        }
+
+        private void LoadShips()
+        {
+            List<SO_shipData> shipList = GetComponent<ServerDB>().GetAllShips();
+            for (int i = 0; i < shipList.Count; i++)
+            {
+                Debug.Log("add Ship " + i);
+                AddShip(shipList[i]);
+            }
+
+            //Debug.Log("all ship" + shipsDB.shipList.Count);
+            //for (int i = 0; i < shipsDB.shipList.Count; i++)
+            //{
+            //   AddShip (shipsDB.shipList[i]);
+
+            //    //            ships = shipsDB.shipList;
+            //}
+        }
+
+        private void AddShip(SO_shipData ship)
+        {
+            bool foundFlag = false;
+
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if (ships[i].p.SO.id == ship.SO.id) foundFlag = true;
+            }
+
+            if (!foundFlag)
+            {
+                SO_ship s = new SO_ship(ship, this.gameObject);
+                ships.Add(s);
+            }
+            else
+            {
+                Debug.Log("Cant add ship id:" + ship.SO.id + " Because it exists.");
+            }
+        }
+
+        private void DeleteShip(int ship_id)
+        {
+            GetComponent<ServerDB>().DeleteSOShipData(ship_id);
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if (ships[i].p.SO.id == ship_id)
+                {
+                    ships[i].BeforeDestroy();
+                    ships[i] = null;
+                    ships.RemoveAt(i);
+
+                    break;
+                }
+            }
+
+        }
+        private void DestrtoyShip(int ship_id)
+        {
+            GetComponent<ServerSO>().AddContainer(ship_id);
+            DeleteShip(ship_id);
+        }
+        public int RegisterPlayer()
+        {
+            return 0;
+        }
+
+        public void PlayerControl(int player_id, Command player_command, int weapon_equip_num)
+        {
+            SO_ship player = GetPlayer(player_id);
+            switch (player_command)
+            {
+                case Command.MoveTo:
+                    player.GoToTarget();
+                    break;
+                case Command.WarpTo:
+                    player.WarpToTarget();
+                    break;
+                case Command.Atack:
+                    player.Atack_target(weapon_equip_num);
+                    break;
+                case Command.LandTo:
+                    player.LandToTarget();
+                    break;
+                case Command.Equipment:
+                    player.StartEquipment();
+                    break;
+                case Command.Open:
+                    player.OpenTarget();
+                    break;
+                case Command.TakeOff:
+                    player.landed = false;
+                    break;
+            }
+        }
+
+        //	public void PlayerControlSetTargetShip(int player_id,Command player_command ,SO_ship target) {
+        //		SO_ship player = GetPlayer(player_id);
+        //		SO_ship tg = GetPlayer(target.p.SO.id);
+        //		if (player_command==Command.SetTarget){
+        //			player.SetTarget(tg);
+        //		}
+        //	}
+        public void PlayerControlSetTarget(int player_id, Command player_command, ServerObject target)
+        {
+            SO_ship player = GetPlayer(player_id);
+            if (player_command == Command.SetTarget)
+            {
+                player.SetTarget(target);
+            }
+
+        }
+
+
+        private void Warp(SO_ship ship)
+        {
+            if (ship.moveCommand == SO_ship.MoveType.warp)
+            {
+                if (ship.Rotate())
+                {
+                    if (!ship.warpCoroutineStarted)
+                    {
+                        StartCoroutine(ship.Warpdrive());
+                    }
+                }
+            }
+        }
+        public void Events(SO_ship.ShipEvenentsType shipEvent, SO_ship ship)
+        {
+            switch (shipEvent)
+            {
+                case SO_ship.ShipEvenentsType.stop:
+                    //                Debug.Log("server" + ship.p.SO.visibleName + " ship stopped");
+
+                    break;
+                case SO_ship.ShipEvenentsType.move:
+                    Debug.Log("server" + ship.p.SO.visibleName + " ship accelerated");
+                    break;
+                case SO_ship.ShipEvenentsType.warmwarp:
+                    Debug.Log("server" + ship.p.SO.visibleName + " ship preparing to warp");
+                    break;
+                case SO_ship.ShipEvenentsType.warp:
+                    Debug.Log("server" + ship.p.SO.visibleName + " ship warping....");
+
+                    break;
+                case SO_ship.ShipEvenentsType.spawn:
+                    Debug.Log("server" + ship.p.SO.visibleName + " ship spawn");
+                    break;
+                case SO_ship.ShipEvenentsType.land:
+                    //			Debug.Log ("server" + ship.p.SO.visibleName + " ship landing " + ship.targetToMove.id);
+                    //			GetComponent<SkillsDB> ().GetAllSkills ();
+                    GetComponent<LandingServer>().Landing(ship.targetToMove.id, ship.p.SO.id);
+                    ship.landed = true;
+                    break;
+                case SO_ship.ShipEvenentsType.destroyed:
+                    StopAllCoroutines(ship);
+                    DestrtoyShip(ship.p.SO.id);
+                    break;
+                case SO_ship.ShipEvenentsType.open:
+                    //			Debug.Log ("server" + ship.p.SO.visibleName + " ship landing " + ship.targetToMove.id);
+                    //			DO NOTHING
+
+                    break;
+
+            }
+        }
+
+        private void Atack(SO_ship ship)
+        {
+            if (ship.atack)
+            {
+                for (int i = 0; i < ship.weapons.Count; i++)
+                {
+                    if (ship.weapons[i].fire && !ship.weapons[i].activated)
+
+                    {
+                        //					Debug.Log (ship.weapons [i].GetHashCode ());
+                        StartCoroutine(ship.weapons[i].Attack());
+                    }
+
+                }
+            }
+        }
+        private void Mine(SO_ship ship)
+        {
+            if (ship.atack)
+            {
+                for (int i = 0; i < ship.weapons.Count; i++)
+                {
+                    if (ship.weapons[i].fire && !ship.weapons[i].activated)
+
+                    {
+                        //					Debug.Log (ship.weapons [i].GetHashCode ());
+                        StartCoroutine(ship.weapons[i].Mine());
+                    }
+
+                }
+            }
+        }
+        private void Equipment(SO_ship ship)
+        {
+            for (int i = 0; i < ship.equipments.Count; i++)
+            {
+                if (ship.equipments[i].activate && !ship.equipments[i].coroutineStarted)
+                {
+                    StartCoroutine(ship.equipments[i].UseEq());
+                }
+            }
+        }
+        private void StopAllCoroutines(SO_ship ship)
+        {
+            for (int i = 0; i < ship.weapons.Count; i++)
+            {
+                if (ship.weapons[i].activated)
+                {
+                    StopCoroutine(ship.weapons[i].atack_co);
+                }
+            }
+            for (int i = 0; i < ship.equipments.Count; i++)
+            {
+                if (ship.equipments[i].activate)
+                {
+                    StopCoroutine(ship.equipments[i].use_co);
+                }
+            }
+
+        }
+
+        private void Tick()
+        {
+            for (int i = 0; i < ships.Count; i++)
+            {
+                ships[i].Tick();
+                Warp(ships[i]);
+                Atack(ships[i]);
+                Equipment(ships[i]);
+                Mine(ships[i]);
+            }
+        }
+
+
+
+
+    }
+}
