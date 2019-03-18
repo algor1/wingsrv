70852010e010888313325383072e56e67bf1eed0
algor1 <alexeev.igor.v@gmail.com>
2019-03-18 17:59:29 +0300

    no message

--- WingSrv/InventoryItem.cs
+++ WingSrv/InventoryItem.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 [System.Serializable] 
 public class InventoryItem
 {
-	public int player_id;
-    public int item_id;
-    public int tech;
-    public int quantity;
+    public int player_id { get; set; }
+    public int item_id { get; set; }
+    public int tech { get; set; }
+    public int quantity { get; set; }
+
+
+
+
 }
