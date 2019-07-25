48d47720f708ea77654e1eeffd2ee25831bcfdcd
algor1 <alexeev.igor.v@gmail.com>
2019-07-25 17:35:41 +0300

    no message

--- WingSrv/ServerManager.cs
+++ WingSrv/ServerManager.cs
@@ -10,15 +10,15 @@ namespace Wingsrv
     public class ServerManager
     {
         public  Server server;
-        public  ServerDB serverDB;
+        //public  ServerDB serverDB;
         public  InventoryServer inventoryServer;
-        public  ItemDB itemDB;
+        //public  ItemDB itemDB;
 
         public  void Run()
         {
-            itemDB = new ItemDB(this);
+            //itemDB = new ItemDB(this);
             inventoryServer = new InventoryServer(this);
-            serverDB = new ServerDB(this);
+            //serverDB = new ServerDB(this);
             server = new Server(this);
             Thread myThread = new Thread(new ThreadStart(server.RunServer));
             myThread.Start();
